7b1db6ebef72fb10b721c70887c84c55950a87af
Hammed Lawal <lawalolatunde14@gmail.com>
2025-12-07 18:37:18 -0500

    test: add CarBookingTest for CarBookingService.bookCars and add JUnit dependency to pom.xml

---
+++ com.bookyourride/src/test/java/com/devlawal/booking/CarBookingTest.java
@@ -0,0 +1,21 @@
+package com.devlawal.booking;
+
+import com.devlawal.car.CarDAO;
+import com.devlawal.car.CarService;
+import com.devlawal.user.User;
+import org.junit.Test;
+
+import java.util.UUID;
+
+import static org.junit.Assert.assertNotNull;
+
+public class CarBookingTest {
+
+    @Test
+    public void canItBookCar() {
+        CarBookingDAO carBookingDAO = new CarBookingDAO();
+        CarService carService = new CarService();
+        CarBookingService carBookingService = new CarBookingService(carBookingDAO, carService);
+        assertNotNull(carBookingService.bookCars(new User(UUID.fromString("7e4b9220-a47a-45a7-a33b-7182ee0dc30e"), "Leila"), "1234"));
+    }
+}
